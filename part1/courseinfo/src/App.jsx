7dcb19bde1c0e92881a97cc50dfc0ffc0cd8c207
Moh Derajatul Ula <derajatul@gmail.com>
2025-09-19 10:41:21 +0700

    Refactor Content component to use Part component for rendering exercises

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -41,21 +41,23 @@ const Content = ({
 }) => {
   return (
     <div>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
+      <Part part={part1} exercises={exercises1} />
+      <Part part={part2} exercises={exercises2} />
+      <Part part={part3} exercises={exercises3} />
     </div>
   );
 };
 
 const Total = ({ exercises1, exercises2, exercises3 }) => {
   return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+};
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
 };
 
 export default App;
