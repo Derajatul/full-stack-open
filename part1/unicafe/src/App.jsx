f7470f8ef6ed490133af0acadebbc4062d1fb812
Moh Derajatul Ula <derajatul@gmail.com>
2025-09-19 13:42:04 +0700

    Refactor App component to implement feedback functionality and update main entry point

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,54 +1,48 @@
+import { useState } from "react";
+
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      {good === 0 && neutral === 0 && bad === 0 ? (
+        <p>No feedback given</p>
+      ) : (
+        <table>
+          <tbody>
+            <Statistic text="good" value={good} />
+            <Statistic text="neutral" value={neutral} />
+            <Statistic text="bad" value={bad} />
+            <Statistic text="all" value={good + neutral + bad} />
+            <Statistic
+              text="average"
+              value={(good - bad) / (good + neutral + bad)}
+            />
+            <Statistic
+              text="positive"
+              value={(good / (good + neutral + bad)) * 100 + " %"}
+            />
+          </tbody>
+        </table>
+      )}
     </div>
   );
 };
 
-const Header = ({ course }) => {
-  console.log(course);
-  return <h1>{course}</h1>;
-};
-
-const Content = ({ parts }) => {
+const Statistic = ({ text, value }) => {
   return (
-    <div>
-      {parts.map((part) => (
-        <Part key={part.name} part={part.name} exercises={part.exercises} />
-      ))}
-    </div>
-  );
-};
-
-const Total = ({ parts }) => {
-  const totalExercises = parts.reduce((sum, part) => sum + part.exercises, 0);
-  return <p>Number of exercises {totalExercises}</p>;
-};
-
-const Part = ({ part, exercises }) => {
-  return (
-    <p>
-      {part} {exercises}
-    </p>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   );
 };
 
