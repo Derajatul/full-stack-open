21d1f41df52e3c16c714e8400febfa0df38ec91e
Moh Derajatul Ula <derajatul@gmail.com>
2025-09-19 11:53:22 +0700

    Refactor App and main components for improved structure and clarity

--- part1/courseinfo/src/main.jsx
+++ part1/courseinfo/src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+let counter = 1;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+const refresh = () => {
+  root.render(<App counter={counter} />);
+};
+
+refresh();
+counter += 1;
+refresh();
+counter += 1;
+refresh();
